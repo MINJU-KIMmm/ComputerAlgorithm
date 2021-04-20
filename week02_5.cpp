7c006a2477cbe020c5855eb54d0a117f2b1f1deb
MINJU_KIMmm <0112km@ewhain.net>
2021-04-20 23:10:24 +0900

    Create week02_5.cpp

---
+++ week02_5.cpp
@@ -0,0 +1,35 @@
+//3.기초데이터구조_2
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 5
+
+void zigzag(int A[][SIZE]) {
+	int value = 1;
+	for (int i = 0; i < SIZE; i++) {
+		if(i%2==0)
+			for (int j = 0; j < SIZE; j++) {
+				A[i][j] = value;
+				value++;
+			}
+		else
+			for (int j = SIZE - 1; j >= 0; j--) {
+				A[i][j] = value;
+				value++;
+			}
+	}
+}
+
+void printArray(int A[][SIZE]) {
+	for (int i = 0; i < SIZE; i++) {
+		for (int j = 0; j < SIZE; j++) 
+			printf("%2d ", A[i][j]);
+		printf("\n");
+	}
+}
+int main() {
+	int A[SIZE][SIZE] = { 0 };
+	zigzag(A);
+	printArray(A);
+}
